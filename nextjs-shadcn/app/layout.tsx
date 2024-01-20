2995557ccc825f8b51c41ef43c97ca8093734250
Luis Mata Salvatierra <luismatasalvatierra@gmail.com>
2024-01-20 18:49:11 +0100

    Update font imports and add Geist package

--- nextjs-shadcn/app/layout.tsx
+++ nextjs-shadcn/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistMono.className}>{children}</body>
     </html>
   );
 }
